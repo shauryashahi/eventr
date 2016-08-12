56977fe2f26293514a1969256c030d8df4cac016
Shaurya <shahi.shaurya@gmail.com>
2016-08-12 14:46:34 +0530

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_init_active_record, false  # Change to true if using ActiveRecord
 
 ## Defaults:
 # set :scm,           :git
