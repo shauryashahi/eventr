ad718ecba36caad2468b996e64fabf4487961770
Shaurya <shahi.shaurya@gmail.com>
2016-08-12 14:32:00 +0530

    deploy bug fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server '52.42.195.255', port: 3000, user: 'deploy', roles: %w{web app db}
+server '52.42.195.255', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
