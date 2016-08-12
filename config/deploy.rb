0bc64148f2c6e1b359d49ac3dbb6e544666bf4f1
Shaurya <shahi.shaurya@gmail.com>
2016-08-12 15:54:49 +0530

    Capistrano deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-
 set :repo_url,        'git@github.com:shauryashahi/eventr.git'
 set :application,     'eventr'
 set :user,            'deploy'
@@ -8,7 +7,6 @@ set :puma_workers,    0
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server '52.42.195.255', user: 'deploy', roles: %w{web app db}
+server '52.26.148.176', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
