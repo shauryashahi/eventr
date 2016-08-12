d9de64bf39768ace82a486d47635cb6b741b25ac
Shaurya <shahi.shaurya@gmail.com>
2016-08-12 16:32:20 +0530

    capist

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+server 'server', user: 'deploy', roles: %w{web app db}, primary: true
+
 set :repo_url,        'git@github.com:shauryashahi/eventr.git'
 set :application,     'eventr'
 set :user,            'deploy'
@@ -14,7 +16,6 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, false  # Change to true if using ActiveRecord
