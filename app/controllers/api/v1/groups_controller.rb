221cdb2a8b71202b14736346b9594dc34ade8f63
Shaurya <shahi.shaurya@gmail.com>
2016-08-29 20:24:47 +0530

    back to basics

--- app/controllers/api/v1/groups_controller.rb
+++ app/controllers/api/v1/groups_controller.rb
@@ -1,44 +1,43 @@
 module Api::V1
   class GroupsController < ApiController
-    # before_action :set_group, only: [:group_members, :destroy, :confirm_member, :join_group, :invite_members]
+    before_action :set_group, only: [:group_members, :destroy, :confirm_member, :join_group, :invite_members]
 
     def event_groups
       if params[:fb_event_id].present?
-        # groups = Group.where(:fb_event_id=>params[:fb_event_id])
-        # render json: {:data=>groups.as_json,:message => "Success"}, status: 200
-        render json: {:data=>Constants::DUMMY_GROUPS, :message=>"Success"}, status: 200
+        groups = Group.where(:fb_event_id=>params[:fb_event_id])
+        render json: {:data=>groups.as_json,:message => "Success"}, status: 200
       else
         render json: {:message => "Cannot find FB Event ID. Check Input Parameters."}, status: 400
       end
     end
 
     def user_groups
-      # render json: {:data=>@current_user.groups.as_json,:message=>"Success"}, status: 200
-      render json: {:data=>Constants::DUMMY_GROUPS, :message=>"Success"}, status: 200
+      render json: {:data=>@current_user.groups.as_json,:message=>"Success"}, status: 200
     end
 
     def group_members
-      # render json: {:data=>@group.users.except(:fb_token).as_json,:message=>"Success"}, status: 200
-      render json: {:data=>Constants::DUMMY_GROUP_MEMBERS, :message=>"Success"}, status: 200
+      render json: {:data=>@group.users.except(:fb_token).as_json,:message=>"Success"}, status: 200
     end
 
-    # def create
-    # end
+    def create
+    end
 
-    # def invite_members
-    # end
+    def invite_members
+    end
 
-    # def join_group
-    # end
+    def join_group
+    end
 
-    # def confirm_member
-    # end
+    def confirm_member
+    end
 
-    # def destroy
-    #   if @group.destroy
-    #     render json: {:message=>"Success"}, status: 200
-    #   end
-    # end
+    def destroy
+      if @group.destroy
+        render json: {:message=>"Success"}, status: 200
+      else
+        render json: {:message=>"#{@group.errors.full_messages}"}, status: 400
+      end
+    end
 
     private
 
